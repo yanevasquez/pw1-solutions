509a8355a24e6c22758802549d9ae8633b660cd0
Yane Vasquez <yanelisset4@gmail.com>
2019-09-04 08:17:11 -0300

    fix and up

--- php-core/basic-harmonic-series/yane.lisset.php
+++ php-core/basic-harmonic-series/yane.lisset.php
@@ -1,36 +1,8 @@
 <?php
 
-//numbers-for
-for ($i = 0; $i < 100; $i++) {
-    $perc= $i."  ";
-    if  ($i%10==0){
-        echo"\n";
-    }
-    if ($i<10) {
-        $perc="0".$i."  ";
-    }
-echo "$perc";
+const size = 10;
+$ac = 0;
+for ($i = 1; $i <= size; $i++) {
+  $ac += 1 / $i;
 }
-  //numbers-reverse-for
-  for( $i=99; $i>=0; $i-=1 ){
-    $perc= $i."  ";
-    if  ($i%10==9){
-        echo"\n";
-    }
-    if ($i<10) {
-        $perc="0".$i."  ";
-    }
-    echo "$perc";
-  }
-//numbers-reverse-odd-for
-  for( $i=99; $i>=0; $i-=2 ){
-    $perc= $i."  ";
-    if  ($i%10==9){
-        echo" \n";
-    }
-    if ($i<10) {
-        $perc="0".$i."  ";
-    }
-    echo "$perc";
-  }
-
+print("h(10): ${ac}");
