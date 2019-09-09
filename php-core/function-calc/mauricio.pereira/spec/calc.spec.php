cc2062ea40560e5057d6d6b3762fc0bfcaa6c6cf
Maurício Pereira da Costa Junior <39006582+mauriciopcj@users.noreply.github.com>
2019-09-08 21:26:16 -0300

    functions (#13)
    * solution function calc
    
    * solution area-of-circle
    
    * solution factorial
    
    * solution sum
    
    * solution triangle-checker
    
    * habilitando demais testes em function-calc
    
    * habilitando um teste de function-triangle-checker
    
    * solution fibonacci-sequence
    
    * retirando var_dump do código
    
    * solution fibonacci-number
    
    * solution function-prime
    
    * solution function-prime-nth

---
+++ php-core/function-calc/mauricio.pereira/spec/calc.spec.php
@@ -0,0 +1,20 @@
+<?php
+require __DIR__ . '/../src/mauricio.pereira.php';
+
+describe('Calculator', function () {
+  it('adding 1 + 1', function () {
+    expect(calc(1, 1, '+'))->toBe(2);
+  });
+
+  it('subtracting 1 - 1', function () {
+    expect(calc(1, 1, '-'))->toBe(0);
+  });
+
+  it('multiplying 1 * 1', function () {
+    expect(calc(1, 1, '*'))->toBe(1);
+  });
+  it('dividing 1 / 1', function () {
+    expect(calc(1, 1, '/'))->toBe(1);
+  });
+});
+ 
